212c91fb73412b488bf369f870856b7c66b9b3b2
netoudi <netoudi@rocketmail.com>
2021-12-10 09:53:57 -0300

    Add usecase ProcessTransaction

---
+++ usecase/process_transaction/process_transaction.go
@@ -0,0 +1,61 @@
+package process_transaction
+
+import (
+	"github.com/netoudi/codepay-process-transaction/domain/entity"
+	"github.com/netoudi/codepay-process-transaction/domain/repository"
+)
+
+type ProcessTransaction struct {
+	Repository repository.TransactionRepository
+}
+
+func NewProcessTransaction(repository repository.TransactionRepository) *ProcessTransaction {
+	return &ProcessTransaction{Repository: repository}
+}
+
+func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoOutput, error) {
+	transaction := entity.NewTransaction()
+	transaction.ID = input.ID
+	transaction.AccountID = input.AccountID
+	transaction.Amount = input.Amount
+
+	cc, invalidCC := entity.NewCreditCard(input.CreditCardNumber, input.CreditCardName, input.CreditCardExpirationMonth, input.CreditCardExpirationYear, input.CreditCardCVV)
+	if invalidCC != nil {
+		err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.REJECTED, invalidCC.Error())
+		if err != nil {
+			return TransactionDtoOutput{}, err
+		}
+		output := TransactionDtoOutput{
+			ID:           transaction.ID,
+			Status:       entity.REJECTED,
+			ErrorMessage: invalidCC.Error(),
+		}
+		return output, nil
+	}
+
+	transaction.SetCreditCard(*cc)
+	invalidTransaction := transaction.IsValid()
+	if invalidTransaction != nil {
+		err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.REJECTED, invalidTransaction.Error())
+		if err != nil {
+			return TransactionDtoOutput{}, err
+		}
+		output := TransactionDtoOutput{
+			ID:           transaction.ID,
+			Status:       entity.REJECTED,
+			ErrorMessage: invalidTransaction.Error(),
+		}
+		return output, nil
+	}
+
+	err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.APPROVED, "")
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	output := TransactionDtoOutput{
+		ID:           transaction.ID,
+		Status:       entity.APPROVED,
+		ErrorMessage: "",
+	}
+	return output, nil
+}
